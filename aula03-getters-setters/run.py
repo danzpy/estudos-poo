f334ef6bea764b61a1abf1a94e916c7e7116e881
Dan <daniel.leonssio@hotmail.com>
2025-04-05 08:22:47 -0300

    Revisão da aula 4

--- aula03-getters-setters/run.py
+++ aula03-getters-setters/run.py
@@ -1,13 +1,13 @@
 class MinhaClasse():
 
     def __init__(self) -> None:
-        self.__valor = None
+        self.__valor = None # Foi definido um atributo privado.
 
 
-    def setter(self, value) -> None:
+    def setter(self, value) -> None: # Um setter é um metodo para definir esse atributo dentro da classe.
         self.__valor = value
 
-    def getter(self) -> int:
+    def getter(self) -> int: # Um getter é um atributo que retorna o que foi definido no setter, também dentro da classe.
         return self.__valor
 
 classe = MinhaClasse()
